b1db545c0c80a597da8d6169de00439acb5069a1
jaewan shin <wodhks6307@naver.com>
2022-02-09 22:38:35 +0900

    달력 수정

--- src/main/resources/static/js/user/join.js
+++ src/main/resources/static/js/user/join.js
@@ -1,33 +1,45 @@
 /*----------------------------------- 달력 생성 ----------------------------------------*/
-window.onload = function () { //처음페이지가 열릴때 동적으로 날짜를 select박스에 넣는다.
-    let sYear = 1980;
-    let eYear = 2022;
-    let sMonth = 1;
-    let eMonth = 12;
-    let sDay = 1;
-    let eDay = 31;
+const cmbyear = document.querySelector('#cmbyear');
+const cmbmonth = document.querySelector('#cmbmonth');
+const cmbday = document.querySelector('#cmbday');
 
-    let strYear = '';
-    let strMonth = '';
-    let strDay = '';
-
-    for (let i = eYear; i >= sYear; i--) { //select랑 option같이쓴다.
-        strYear += "<option value=" + i + ">" + i + "</option>";
-    }
-
-    for (let i = sMonth; i <= eMonth; i++) {
-        strMonth += "<option value=" + i + ">" + i + "</option>";
-    }
-
-    for (let i = sDay; i <= eDay; i++) {
-        strDay += "<option value=" + i + ">" + i + "</option>";
-    }
-
-    document.getElementById("year").innerHTML = strYear;
-    document.getElementById("month").innerHTML = strMonth;
-    document.getElementById("day").innerHTML = strDay;
+for (let i = 1970; i <= 2022; i++) {
+    cmbyear.options[i - 1970] = new Option(i, i); //new option(실제값, value)
 }
 
+for (let i = 0; i < 12; i++) {
+    cmbmonth.options[i] = new Option(i + 1, i);
+}
+
+cmbyear.options[new Date().getFullYear() - 1970].selected = "seleted"
+cmbmonth.options[new Date().getMonth()].selected = "seleted" //getMonth() : 1월~12월 > 리턴0~11
+
+function getLastDay(year, month) {
+    let lastDay = new Date(year, month + 1);
+    lastDay.setDate(0);
+    // console.log(lastDay.getDate()); //28
+    return lastDay.getDate(); //마지막날짜 리턴
+    //ex : 2월달의 마지막일을 알고싶다면 XX년 1월 1일로 설정한 후 setDate에 인자로 0을 주면 된다.
+    // Date객체는 알아서 월간 이동하고 2월의 마지막일로 설정된다. getDate()는 일(몇일)을 가져옴
+}
+
+function set_day() {
+    let year = parseInt(cmbyear.options[cmbyear.selectedIndex].value); //value는 기본이 문자열이라 숫자로 형변환
+    let month = parseInt(cmbmonth.options[cmbmonth.selectedIndex].value); //실제값말고 value가져옴
+    //날짜간 덧셈, 뺄샘 과정이 일어날 수 있음으로 number로 형변환
+    // console.log(year, month); //2022, 1
+    let lastDay = getLastDay(year, month);
+    for (let i = 0; i <= lastDay - 1; i++) {
+        cmbday.options[i] = new Option(i + 1, i + 1);
+    }
+}
+
+set_day();
+
+function m_change() {
+    cmbday.options.length = 0;
+    set_day();
+}
 /*---------------------------- 조건 체크 -----------------------------------*/
 const form = document.querySelector('form');
 if (form) {
@@ -35,28 +47,28 @@ if (form) {
     const pwRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,15}$/;
     const blank_pattern = /[\s]/g; //공백금지
     form.addEventListener('submit', (e) => {
-        if(form.checkId.value === '' || blank_pattern.test(form.checkId.value) == true){ //공백이있다면 true리턴
+        if (form.checkId.value === '' || blank_pattern.test(form.checkId.value) == true) { //공백이있다면 true리턴
             e.preventDefault();
             alert('체크된 항목은 필수사항입니다.')
-        } else if(form.passCheck.value === '' || blank_pattern.test(form.passCheck.value) == true){
+        } else if (form.passCheck.value === '' || blank_pattern.test(form.passCheck.value) == true) {
             e.preventDefault();
             alert('체크된 항목은 필수사항입니다.')
-        } else if(form.rePassCheck.value === '' || blank_pattern.test(form.rePassCheck.value) == true){
+        } else if (form.rePassCheck.value === '' || blank_pattern.test(form.rePassCheck.value) == true) {
             e.preventDefault();
             alert('체크된 항목은 필수사항입니다.')
-        } else if(form.nmCheck.value === '' || blank_pattern.test(form.nmCheck.value) == true){
+        } else if (form.nmCheck.value === '' || blank_pattern.test(form.nmCheck.value) == true) {
             e.preventDefault();
             alert('체크된 항목은 필수사항입니다.')
-        } else if(form.pNumberCheck.value === '' || blank_pattern.test(form.pNumberCheck.value) == true){
+        } else if (form.pNumberCheck.value === '' || blank_pattern.test(form.pNumberCheck.value) == true) {
             e.preventDefault();
             alert('체크된 항목은 필수사항입니다.')
-        } else if(!pwRegex.test(form.passCheck.value)){
+        } else if (!pwRegex.test(form.passCheck.value)) {
             e.preventDefault();
             alert('비밀번호는 영문자, 숫자, 특수문자 포함 8~15글자입니다.')
-        } else if(form.passCheck.value !== form.rePassCheck.value){
+        } else if (form.passCheck.value !== form.rePassCheck.value) {
             e.preventDefault();
             alert('비밀번호가 서로 다릅니다.')
-        } else if(!form.agree.checked){
+        } else if (!form.agree.checked) {
             e.preventDefault();
             alert('약관에 동의해주세요.')
         }
