b658df60cabfeb7ad7d45cd27c2f9a57e73556ab
jaewan shin <wodhks6307@naver.com>
2022-02-21 22:28:06 +0900

    카카오 로그인 구현

--- src/main/java/com/koreait/vehicleservice/kakao/KaKaoService.java
+++ src/main/java/com/koreait/vehicleservice/kakao/KaKaoService.java
@@ -1,5 +1,8 @@
 package com.koreait.vehicleservice.kakao;
 
+import com.koreait.vehicleservice.user.UserEntity;
+import com.koreait.vehicleservice.user.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonElement;
@@ -12,106 +15,15 @@ import java.net.URL;
 @Service
 public class KaKaoService {
 
-    public String getKaKaoAccessToken(String code) {
-        String access_Token = "";
-        String refresh_Token = "";
-        String reqURL = "https://kauth.kakao.com/oauth/token";
+    @Autowired UserMapper userMapper;
 
-        try{
-            URL url = new URL(reqURL);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-
-            //POST 요청을 위해 기본값이 false인 setDoOutput을 true로
-            conn.setRequestMethod("POST");
-            conn.setDoOutput(true);
-
-            //POST 요청에 필요로 요구하는 파라미터 스트림을 통해 전송
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
-            StringBuilder sb = new StringBuilder();
-            sb.append("grant_type=authorization_code");
-            sb.append("&client_id=0e08b6c96d5f791a6eb59bd54c31aa90"); // TODO REST_API_KEY 입력
-            sb.append("&redirect_uri=http://localhost:8090/user/login"); // TODO 인가코드 받은 redirect_uri 입력
-            sb.append("&code=" + code);
-            bw.write(sb.toString());
-            bw.flush();
-
-            //결과 코드가 200이라면 성공
-            int responseCode = conn.getResponseCode();
-            System.out.println("responseCode : " + responseCode);
-            //요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
-            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            String line = "";
-            String result = "";
-
-            while ((line = br.readLine()) != null) {
-                result += line;
-            }
-            System.out.println("response body : " + result);
-
-            //Gson 라이브러리에 포함된 클래스로 JSON파싱 객체 생성
-            JsonParser parser = new JsonParser();
-            JsonElement element = parser.parse(result);
-
-            access_Token = element.getAsJsonObject().get("access_token").getAsString();
-            refresh_Token = element.getAsJsonObject().get("refresh_token").getAsString();
-
-            System.out.println("access_token : " + access_Token);
-            System.out.println("refresh_token : " + refresh_Token);
-
-            br.close();
-            bw.close();
-        }catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return access_Token;
+    public void kaoJoin(UserEntity kaoUser){
+        userMapper.kaoInsUser(kaoUser);
     }
 
-    public void createKakaoUser(String token) throws Exception {
-
-        String reqURL = "https://kapi.kakao.com/v2/user/me";
-
-        //access_token을 이용하여 사용자 정보 조회
-        try {
-            URL url = new URL(reqURL);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-
-            conn.setRequestMethod("POST");
-            conn.setDoOutput(true);
-            conn.setRequestProperty("Authorization", "Bearer " + token); //전송할 header 작성, access_token전송
-
-            //결과 코드가 200이라면 성공
-            int responseCode = conn.getResponseCode();
-            System.out.println("responseCode : " + responseCode);
-
-            //요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
-            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            String line = "";
-            String result = "";
-
-            while ((line = br.readLine()) != null) {
-                result += line;
-            }
-            System.out.println("response body : " + result);
-
-            //Gson 라이브러리로 JSON파싱
-            JsonParser parser = new JsonParser();
-            JsonElement element = parser.parse(result);
-
-            int id = element.getAsJsonObject().get("id").getAsInt();
-            boolean hasEmail = element.getAsJsonObject().get("kakao_account").getAsJsonObject().get("has_email").getAsBoolean();
-            String email = "";
-            if(hasEmail){
-                email = element.getAsJsonObject().get("kakao_account").getAsJsonObject().get("email").getAsString();
-            }
-
-            System.out.println("id : " + id);
-            System.out.println("email : " + email);
-
-            br.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public UserEntity kaoUserFind(String id){
+        UserEntity entity = new UserEntity();
+        entity.setUid(id);
+        return userMapper.selUser(entity);
     }
 }
