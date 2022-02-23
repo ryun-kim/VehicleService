e6bd29b74b5da5c69f3a7a4759c7db7ae77d0229
ryun <kjr6260@naver.com>
2022-02-23 09:34:22 +0900

    Revert "02-22"
    This reverts commit fe9385f9eee1aad51542a0b407ec0417804101e1.

--- src/main/resources/static/js/vehicle/list.js
+++ src/main/resources/static/js/vehicle/list.js
@@ -1,5 +1,7 @@
 {
     const searchFrmElem = document.querySelector('#search_Result_Frm'); //검색어입력창
+    const jjimElemArr = document.querySelectorAll('.jjimBtn'); //찜버튼
+    const listorderElem = document.querySelector('#list_order'); // 정렬방법 만들어야함!!!
 
     if (searchFrmElem) { //검색창에 검색없으면 알람
         searchFrmElem.addEventListener('submit', () => {
@@ -12,18 +14,22 @@
         });
     }
 
-    //찜버튼 활성화
-    function jjimEvent(pk, target){
-        console.log(target);
-        if (target.firstChild.classList.contains('fa-solid')) {
-            target.firstChild.classList.remove('fa-solid');
-            target.firstChild.classList.add('fa-regular');
-            target.classList.remove('btn-outline-danger');
-        } else {
-            target.firstChild.classList.remove('fa-regular');
-            target.firstChild.classList.add('fa-solid');
-            target.classList.add('btn-outline-danger');
-        }
+    if (jjimElemArr) { //좋아요버튼 활성화
+        jjimElemArr.forEach(item => {
+            const jjimHeart = item.firstChild;
+
+            item.addEventListener('click', () => {
+                if (jjimHeart.classList.contains('fa-solid')) {
+                    jjimHeart.classList.remove('fa-solid');
+                    jjimHeart.classList.add('fa-regular');
+                    item.classList.remove('btn-outline-danger');
+                } else {
+                    jjimHeart.classList.remove('fa-regular');
+                    jjimHeart.classList.add('fa-solid');
+                    item.classList.add('btn-outline-danger');
+                }
+            });
+        })
     }
 
     //글 리스트 정보 가져오기
@@ -52,7 +58,7 @@
                     </div>
                     <div class="g-4">
                         <div class="d-flex justify-content-end align-items-center">
-                        <button type="button" class="btn jjimBtn" onclick="jjimEvent(item.selliboard, this);"><i class="fa-regular fa-heart"></i>좋아요</button>
+                        <button type="button" class="btn jjimBtn"><i class="fa-regular fa-heart"></i>좋아요</button>
                         </div>
                     </div>
                 </div>
